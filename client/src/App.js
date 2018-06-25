16b3949a94ccb21268f1e7830fca6c8dcce12867
Chandan Kumar <chandanchaudharyrules@gmail.com>
2018-06-25 21:13:17 +0530

    Create react app using create-react-app cli Wrap the application inside graph apollo library

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import BookList from './components/BookList';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Book Store</h1>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
